2271a3e938a6cecec72efc4926a9968ce816bb57
nucweacia94fine <40483784+nucweacia94fine@users.noreply.github.com>
2020-12-20 20:51:45 +0800

    Add files via upload

--- program_restart_except_3_in_3min.py
+++ program_restart_except_3_in_3min.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[22]:
+# In[25]:
 
 
 import datetime
@@ -17,6 +17,8 @@ def nest2():
 
 def main():
     nest2()
+    
+record_filename = "Error_record_file.txt"
 
 if __name__=="__main__":
     sys_error_count = 0  
@@ -26,7 +28,6 @@ if __name__=="__main__":
             main()
         except Exception as e:
             cl, exc, tb = sys.exc_info() #取得Call Stack
-            #lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
             DT_error = datetime.datetime.now()
             print("")       
             print(f"This error occurred time: {DT_error.replace(microsecond = 0)}")
@@ -36,23 +37,41 @@ if __name__=="__main__":
             #print(e)
             #print("")
             #print(traceback.extract_tb(tb))
-            for lastCallStack in traceback.extract_tb(tb):
+            
+            f_rcd = open(record_filename, "a", encoding='utf-8')
+            print("", file = f_rcd)
+            print(f"This error occurred time: {DT_error.replace(microsecond = 0)}", file = f_rcd)
+            print(f"Last error occurred time: {DT_error2.replace(microsecond = 0)}", file = f_rcd)
+            print("---------------------------------------------------------------------", file = f_rcd)
+            print(f"main() error due to: {type(e)}", file = f_rcd)
+            
+            #lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            for lastCallStack in traceback.extract_tb(tb): # 依序取得Call Stack的所有資料
                 err_fileName = lastCallStack[0] #取得發生的檔案名稱
                 err_lineNum = lastCallStack[1] #取得發生的行號
                 err_funcName = lastCallStack[2] #取得發生的函數名稱
                 err_Msg = f"\nFile \"{err_fileName}\",\n--> line {err_lineNum}, in {err_funcName}:"
                 # Ref.[1]     
                 print(err_Msg)
+                print(err_Msg, file = f_rcd)
             print(f"[{type(e)}]: {e.args[0]}")
+            print(f"[{type(e)}]: {e.args[0]}", file = f_rcd)
+            f_rcd.close()
             
             if sys_error_count == 2: 
                 print("\n!!!   Error occurred too many times in 3 minutes. !!!\n")
                 print("=====================================================================")
                 
+                f_rcd = open(record_filename, "a", encoding='utf-8')
+                print("\n!!!   Error occurred too many times in 3 minutes. !!!\n", file = f_rcd)
+                print("=====================================================================", file = f_rcd)
+                f_rcd.close()
                 raise;
             else:
                 print("=====================================================================")
-                               
+                f_rcd = open(record_filename, "a", encoding='utf-8')
+                print("=====================================================================", file = f_rcd)
+                f_rcd.close()               
                 if e.args[0] == 28: # 硬碟容量不足
                     win32api.ShellExecute(0, 'open', '釋放C槽空間.bat', '','',1)
                 
